a48274a3e18ba1c254a52bd194bcc7bda5291894
Saptadeep Banerjee <saptadeep555@gmail.com>
2024-11-21 07:57:55 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -29,7 +29,7 @@ graph = {
 	'C': {'A': 4, 'B': 2, 'D': 1},	while priority_queue: 
 	'D': {'B': 5, 'C': 1}	# Graph as an adjacency list with weights as values of the inner dictionary 	efficiently represent the graph
 } # Graph as an adjacency list with weights as values of the inner dictionary 	efficiently represent the graph
-WindowsError	# Graph as an adjacency list with weights as values of the inner dictionary 	efficiently represent the graph
+WindowsError	# Graph as an adjacency list with weights as values of the inner dictionary 	efficiently represent the graph e.g. {'A': {'B': 1, 'C': 4}, 'B': {'A': 1, 'C': 2, 'D': 5}, 'C': {'A': 4, 'B': 2, 'D': 1}, 'D': {'B': 5, 'C': 1}}
 start_vertex = 'A' # Starting vertex for the algorithm to start fro	wm 			wd		dijkstra(graph, start_vertex)  # Output: {'A': 0, 'B': 1, 'C': 3, 'D': 4}
 print(dijkstra(graph, start_vertex))  # Output: {'A': 0, 'B': 1, 'C': 3, 'D': 4}
 ```
