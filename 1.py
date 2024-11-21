b50933df3bb3f9ac523f68bcf3c265621137b21d
Saptadeep Banerjee <saptadeep555@gmail.com>
2024-11-21 08:00:36 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -21,7 +21,7 @@ def dijkstra(graph, start):
 				heapq.heappush(priority_queue, (distance, neighbor))
 	
 	return distances
-w|# Example usage are: create a graph and call the function as shown below example effefefeefeffcvesddfdfunction as shown below example
+w|# Example usage are: create a graph and call the function as shown below example effefefeefeffcvesddfdfassdunction as shown below example
 graph = {
 	'A': {'B': 1, 'C': 4},
 	'B': {'A': 1, 'C': 2, 'D': 5},
