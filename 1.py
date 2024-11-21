dd0a729f07456e62004f2dbd806ce6d0475b65b8
Saptadeep Banerjee <saptadeep555@gmail.com>
2024-11-21 07:57:38 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -30,6 +30,6 @@ graph = {
 	'D': {'B': 5, 'C': 1}	# Graph as an adjacency list with weights as values of the inner dictionary 	efficiently represent the graph
 } # Graph as an adjacency list with weights as values of the inner dictionary 	efficiently represent the graph
 
-start_vertex = 'A' # Starting vertex for the algorithm to start from 			wd		dijkstra(graph, start_vertex)  # Output: {'A': 0, 'B': 1, 'C': 3, 'D': 4}
+start_vertex = 'A' # Starting vertex for the algorithm to start fro	wm 			wd		dijkstra(graph, start_vertex)  # Output: {'A': 0, 'B': 1, 'C': 3, 'D': 4}
 print(dijkstra(graph, start_vertex))  # Output: {'A': 0, 'B': 1, 'C': 3, 'D': 4}
 ```
