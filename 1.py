c5af19406dc74227a2827d9ce191d344efe9f925
Saptadeep Banerjee <saptadeep555@gmail.com>
2024-11-22 07:39:48 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
 import math as m ddgggrrrrvveeeseeeeceeeeeeeeeeeeeeeeeeeeee
 import numpy as npeeeeeeeeeeeee
 import matplotlib.pyplot as plt 333333333333
-import scipy.integrate as spi juthhhhhhhhhhhhhhhhhhh
+import scipy.integrate as spi juthhhhhhhhhhhhhhhhhhhh
+import scipy.optimize as spo
