82df341bedfddc2f75173733b0b6c311a3cf63eb
Saptadeep Banerjee <saptadeep555@gmail.com>
2024-12-08 09:14:13 +0530

    Update tcs6_Counr_press.cpp

--- tcs6_Counr_press.cpp
+++ tcs6_Counr_press.cpp
@@ -1,6 +1,6 @@
 import math
 def polygon_area(vertices):
-    n = legggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggn(vertices)
+    n = leggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggn(vertices)
     area = 0er5tuxxxxxxxxxxxxxxx
     for i in range(n):
         x1, y1 = vertices[i]
